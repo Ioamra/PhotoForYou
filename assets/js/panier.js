d78070a9d63944e574bee4aca1b751a9bed03770
ioamra <iom.monchau@gmail.com>
2022-03-22 19:48:09 +0100

    pannier responsive

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -2,11 +2,15 @@ $(function(){
     actuPanier();
 });
 function tooglePanier() {
-
     if ($('#panier').attr('style').match('display: none;')) {
         // Agrandir
         $('#panier').show();
-        $('#panier').animate({width: '24%'});
+
+        if (window.screen.width > 1199) $('#panier').animate({width: '32%'});
+        if (window.screen.width > 768 & window.screen.width < 1200) $('#panier').animate({width: '45%'});
+        if (window.screen.width < 768) $('#panier').animate({width: '90%'});
+        
+
     } else {
         // Réduire
         $('#panier').animate({width: '0'}, function(){$('#panier').hide();});
@@ -31,7 +35,11 @@ function actuPanier() {
             contenu += '        </a>';
             contenu += '    </div>';
             contenu += '    <div class="col">';
-            contenu += '        <div class="">'+panier[i]['nom']+'</div>';
+            contenu += '        <svg onclick="tooglePanier()" role="button" style="position:absolute; right:10px;" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">';
+            contenu += '            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>';
+            contenu += '            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>';
+            contenu += '        </svg>';
+            contenu += '        <br><div class="">'+panier[i]['nom']+'</div>';
             contenu += '        <div class="">'+panier[i]['prix']+'</div>';
             contenu += '        <div class="btn btn-sm btn-secondary" role="button" onclick="removePanier({id:'+panier[i]['id']+'});actuPanier();">supprimer<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash3 ps-2" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></div>';
             contenu += '    </div>';
@@ -75,14 +83,21 @@ function getPanier() {
 
 function addPanier(produit) {
     let panier = getPanier();
-    panier.push(produit);
-    savePanier(panier);
+    let produitExistant = panier.find(function(p){ return p.id == produit.id; });
+    if (produitExistant == undefined) {
+        panier.push(produit);
+        savePanier(panier);
+    } else {
+        alert('Cette image est déjà dans votre panier, si vous voulez la supprimer du panier, veuillez le faire via le pannier.');
+        if ($('#panier').attr('style').match('display: none;')) {
+            tooglePanier();
+        }
+    }
 }
 
 function removePanier(produit) {
     let panier = getPanier();
     panier = panier.filter(function(p){ return p.id != produit.id; });
-    console.log(panier.filter(function(p){ return p.id != produit.id; }));
     savePanier(panier);
 }
 
