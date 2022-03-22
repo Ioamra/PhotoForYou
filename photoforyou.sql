bcaa40c09a231b9ad35b9704df7db69434d24990
ioamra <iom.monchau@gmail.com>
2022-03-22 20:04:35 +0100

    bdd

--- photoforyou.sql
+++ photoforyou.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Lun 21 Mars 2022 à 16:18
--- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 22 mars 2022 à 19:04
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `photoforyou`
+-- Base de données : `photoforyou`
 --
 
 -- --------------------------------------------------------
@@ -26,12 +27,14 @@ SET time_zone = "+00:00";
 -- Structure de la table `categorie`
 --
 
-CREATE TABLE `categorie` (
-  `nom_categorie` varchar(20) NOT NULL
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `nom_categorie` varchar(20) NOT NULL,
+  PRIMARY KEY (`nom_categorie`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `categorie`
+-- Déchargement des données de la table `categorie`
 --
 
 INSERT INTO `categorie` (`nom_categorie`) VALUES
@@ -46,18 +49,20 @@ INSERT INTO `categorie` (`nom_categorie`) VALUES
 -- Structure de la table `image`
 --
 
-CREATE TABLE `image` (
-  `id_image` int(11) NOT NULL,
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE IF NOT EXISTS `image` (
+  `id_image` int(11) NOT NULL AUTO_INCREMENT,
   `nom_image` varchar(20) NOT NULL,
   `prix_image` int(11) NOT NULL,
   `chemin_image` varchar(60) NOT NULL,
   `nom_categorie` varchar(60) NOT NULL,
   `id_vendeur` int(11) NOT NULL,
-  `id_acheteur` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `id_acheteur` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_image`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `image`
+-- Déchargement des données de la table `image`
 --
 
 INSERT INTO `image` (`id_image`, `nom_image`, `prix_image`, `chemin_image`, `nom_categorie`, `id_vendeur`, `id_acheteur`) VALUES
@@ -79,19 +84,20 @@ INSERT INTO `image` (`id_image`, `nom_image`, `prix_image`, `chemin_image`, `nom
 -- Structure de la table `navbar`
 --
 
-CREATE TABLE `navbar` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `navbar`;
+CREATE TABLE IF NOT EXISTS `navbar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `acte` varchar(40) NOT NULL,
   `lien` varchar(40) NOT NULL,
-  `droit` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `droit` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `navbar`
+-- Déchargement des données de la table `navbar`
 --
 
 INSERT INTO `navbar` (`id`, `acte`, `lien`, `droit`) VALUES
-(1, 'Acheter des crédits', 'acheter-des-credits.php', 1),
 (3, 'Déposer des images', 'deposer-des-images.php', 2),
 (5, 'Gestion Utilisateur', 'Gestion-Utilisateur.php', 3),
 (6, 'Gestion Catégorie', 'gestion-categorie.php', 3);
@@ -102,8 +108,9 @@ INSERT INTO `navbar` (`id`, `acte`, `lien`, `droit`) VALUES
 -- Structure de la table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(40) NOT NULL,
   `prenom` varchar(40) NOT NULL,
   `pseudo` varchar(20) NOT NULL,
@@ -113,67 +120,22 @@ CREATE TABLE `user` (
   `grade` varchar(40) NOT NULL,
   `SIRET` varchar(14) DEFAULT NULL,
   `credits` int(11) NOT NULL DEFAULT '0',
-  `etat` varchar(15) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `etat` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `user`
+-- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id`, `nom`, `prenom`, `pseudo`, `img_profil`, `mail`, `mdp`, `grade`, `SIRET`, `credits`, `etat`) VALUES
-(1, 'admin', 'admin', 'admin', 'upload/user/defaut.png', 'admin@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'admin', '0', 9999, 'valid'),
-(2, 'photographe', 'photographe', '', 'upload/user/defaut.png', 'photographe@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '01234567890123', 0, 'valid'),
-(3, 'client', 'client', '', 'upload/user/defaut.png', 'client@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'client', '0', 0, 'valid'),
-(5, 'afjehja', 'kfzkenf', '', 'upload/user/defaut.png', 'test@test.te', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '01234567891234', 0, 'banni'),
-(11, 'nom', 'prenom', 'pseudo', 'upload/user/defaut.png', 'test@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '12365478963222', 0, 'valid');
+(1, 'admin', 'admin', 'admin', 'upload/user/defaut.png', 'admin@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'admin', '0', 10019, 'valid'),
+(2, 'Maroud', 'Kevin', 'Lareyku', 'upload/user/defaut.png', 'photographe@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '01234567890123', 0, 'valid'),
+(3, 'Duval', 'Jordan', 'Belorik', 'upload/user/defaut.png', 'client@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'client', '0', 0, 'valid'),
+(5, 'Majou', 'Marie', 'Deyrha', 'upload/user/defaut.png', 'test@test.te', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '01234567891234', 0, 'banni'),
+(11, 'Kazey', 'Jean', 'Kahrean', 'upload/user/defaut.png', 'test@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '12365478963222', 0, 'valid');
+COMMIT;
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`nom_categorie`);
-
---
--- Index pour la table `image`
---
-ALTER TABLE `image`
-  ADD PRIMARY KEY (`id_image`);
-
---
--- Index pour la table `navbar`
---
-ALTER TABLE `navbar`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `image`
---
-ALTER TABLE `image`
-  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT pour la table `navbar`
---
-ALTER TABLE `navbar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
