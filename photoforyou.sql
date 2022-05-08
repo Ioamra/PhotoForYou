aec4be26a3d11b9e3b48e9c23da72ce7f9917921
ioamra <iom.monchau@gmail.com>
2022-05-09 00:15:10 +0200

    maj bdd

--- photoforyou.sql
+++ photoforyou.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 09 avr. 2022 à 03:34
+-- Généré le : Dim 08 mai 2022 à 22:13
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -38,10 +38,9 @@ CREATE TABLE IF NOT EXISTS `categorie` (
 --
 
 INSERT INTO `categorie` (`nom_categorie`) VALUES
-('graffiti'),
-('paysage'),
-('peinture acrylique'),
-('portrait');
+('Animaux'),
+('Océan'),
+('Paysage');
 
 -- --------------------------------------------------------
 
@@ -61,24 +60,27 @@ CREATE TABLE IF NOT EXISTS `image` (
   `date_achat` varchar(10) DEFAULT NULL,
   `heure_achat` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id_image`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `image`
 --
 
 INSERT INTO `image` (`id_image`, `nom_image`, `prix_image`, `chemin_image`, `nom_categorie`, `id_vendeur`, `id_acheteur`, `date_achat`, `heure_achat`) VALUES
-(1, 'Dune', 200, 'upload/graffiti/Dune21-03-22_13-40-45.jpg', 'graffiti', 11, 3, '2022-04-09', '05:16'),
-(2, 'Glacier', 150, 'upload/paysage/Glacier21-03-22_13-41-04.jpg', 'paysage', 11, NULL, NULL, NULL),
-(3, 'Graffeur du futur', 200, 'upload/graffiti/Graffeur-du-futur21-03-22_13-41-20.jpg', 'graffiti', 11, NULL, NULL, NULL),
-(4, 'Graffiti personnage', 200, 'upload/graffiti/Graffiti-personnage21-03-22_13-41-36.jpg', 'graffiti', 11, 3, '2022-04-09', '05:02'),
-(5, 'Change Your Agenda', 200, 'upload/graffiti/Change-Your-Agenda21-03-22_13-42-00.jpg', 'graffiti', 11, NULL, NULL, NULL),
-(6, 'La sainte victoire', 250, 'upload/paysage/La-sainte-victoire21-03-22_13-42-15.jpg', 'paysage', 11, 3, '2022-04-08', '17:08'),
-(7, 'Libellule', 400, 'upload/peinture-acrylique/Libellule21-03-22_13-42-30.jpg', 'peinture acrylique', 11, NULL, NULL, NULL),
-(8, 'Montagne', 200, 'upload/paysage/Montagne21-03-22_13-42-47.jpg', 'paysage', 11, 3, '2022-04-07', '15:13'),
-(9, 'Portrait Street Art', 350, 'upload/portrait/Portrait-Street-Art21-03-22_13-43-16.jpg', 'portrait', 11, NULL, NULL, NULL),
-(10, 'Portrait Street Art', 320, 'upload/portrait/Portrait-Street-Art21-03-22_13-44-43.jpg', 'portrait', 11, NULL, NULL, NULL),
-(11, 'Turfu', 450, 'upload/peinture-acrylique/Turfu21-03-22_13-45-23.jpg', 'peinture acrylique', 11, NULL, NULL, NULL);
+(30, 'Corail', 300, 'upload/Océan/Corail02-05-22_15-56-54.jpg', 'Océan', 2, NULL, NULL, NULL),
+(29, 'Corail étrange', 500, 'upload/Océan/Corail-étrange02-05-22_15-56-41.jpg', 'Océan', 2, NULL, NULL, NULL),
+(28, 'Fond marin', 250, 'upload/Océan/Fond-marin02-05-22_15-56-04.jpg', 'Océan', 2, NULL, NULL, NULL),
+(27, 'Paysage Canadien', 1100, 'upload/Paysage/Paysage-Canadien02-05-22_15-44-52.jpg', 'Paysage', 2, NULL, NULL, NULL),
+(26, 'Arbre d\'australie', 7000, 'upload/Paysage/Arbre-d\'australie02-05-22_15-44-26.jpg', 'Paysage', 2, NULL, NULL, NULL),
+(25, 'Lac d\'hiver', 600, 'upload/Paysage/Lac-d\'hiver02-05-22_15-41-37.jpg', 'Paysage', 2, NULL, NULL, NULL),
+(24, 'Massif de Belledonne', 2200, 'upload/Paysage/Massif-de-Belledonne02-05-22_15-40-25.jpg', 'Paysage', 2, NULL, NULL, NULL),
+(22, 'Aigle', 1700, 'upload/Animaux/Aigle02-05-22_15-03-24.jpg', 'Animaux', 2, NULL, NULL, NULL),
+(21, 'Lion', 300, 'upload/Animaux/Lion02-05-22_15-02-26.jpg', 'Animaux', 2, 3, '2022-05-02', '18:07'),
+(19, 'Renard', 500, 'upload/Animaux/Renard02-05-22_14-59-03.jpg', 'Animaux', 2, NULL, NULL, NULL),
+(20, 'Tigre', 400, 'upload/Animaux/Tigre02-05-22_15-02-09.jpg', 'Animaux', 2, NULL, NULL, NULL),
+(23, 'Forêt d\'automne', 600, 'upload/Paysage/Forêt-d\'automne02-05-22_15-23-07.jpg', 'Paysage', 2, NULL, NULL, NULL),
+(32, 'Corail habité', 650, 'upload/Océan/Corail-habité02-05-22_15-57-36.jpg', 'Océan', 2, NULL, NULL, NULL),
+(31, 'Vague d\'australie', 1400, 'upload/Océan/Vague-d\'australie02-05-22_15-57-13.jpg', 'Océan', 2, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -124,7 +126,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `credits` int(11) NOT NULL DEFAULT '0',
   `etat` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `user`
@@ -132,10 +134,10 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`id`, `nom`, `prenom`, `pseudo`, `img_profil`, `mail`, `mdp`, `grade`, `SIRET`, `credits`, `etat`) VALUES
 (1, 'admin', 'admin', 'admin', 'upload/user/defaut.png', 'admin@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'admin', '0', 9999, 'valid'),
-(2, 'Maroud', 'Kevin', 'Lareyku', 'upload/user/defaut.png', 'photographe@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '01234567890123', 0, 'valid'),
-(3, 'Duval', 'Jordan', 'Belorik', 'upload/user/defaut.png', 'client@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'client', '0', 3300, 'valid'),
-(5, 'Majou', 'Marie', 'Deyrha', 'upload/user/defaut.png', 'test@test.te', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '01234567891234', 0, 'banni'),
-(11, 'Kazey', 'Jean', 'Kahrean', 'upload/user/defaut.png', 'test@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '12365478963222', 1700, 'valid');
+(2, 'Maroud', 'Kevin', 'Lareyku', 'upload/user/defaut.png', 'photographe@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '01234567890123', 300, 'valid'),
+(3, 'Duval', 'Jordan', 'Belorik', 'upload/user/defaut.png', 'client@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'client', '0', 3000, 'valid'),
+(17, 'Dupres', 'Simon', 'Zekles', 'upload/user/Zekles.jpg', 'zekles@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'photographe', '01234567890124', 0, 'attValid'),
+(16, 'Filon', 'Jean', 'Jouloulou', 'upload/user/defaut.png', 'jean@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', 'client', '', 0, 'banni');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
